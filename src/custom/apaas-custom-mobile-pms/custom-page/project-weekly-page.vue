d65be8de4db774ed23899946175e7c059ade6526
Curry3O <hb1461074099@163.com>
2022-01-13 20:41:34 +0800

    upd: 优化

--- src/custom/apaas-custom-mobile-pms/custom-page/project-weekly-page.vue
+++ src/custom/apaas-custom-mobile-pms/custom-page/project-weekly-page.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-12-27 15:35:18
- * @LastEditTime: 2022-01-13 20:17:16
+ * @LastEditTime: 2022-01-13 20:38:07
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \apaas-mobile-pms\src\custom\apaas-custom-mobile-pms\custom-page\project-weekly-page.vue
@@ -336,6 +336,7 @@ export default {
     },
     // 筛选周数
     selectWeek() {
+      this.componentPopup.hide()
       this.datePicker = this.$createDatePicker({
         title: '',
         value: new Date(),
